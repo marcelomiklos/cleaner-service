ec584915f147b184a153ea8b92b599e7339ab9d0
Marcelo Miklos <marcelomiklos@MacBook-Pro.local>
2024-05-05 02:09:03 -0300

    add paths initilizr - fixxing

---
+++ src/main/java/com/cleanerservice/cleanerservice/CleanerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cleanerservice.cleanerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanerServiceApplication.class, args);
+	}
+
+}
